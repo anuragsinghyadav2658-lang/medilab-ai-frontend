aab1cc1f154fc8c373ca97854f2c84f761d0efef
Anurag-Codes <anuragsinghyadav2658@gmail.com>
2026-07-08 17:59:21 +0530

    login with google ko setup kiya

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* NAYA CODE: GoogleOAuthProvider add kiya gaya hai tera Client ID daal ke */}
+    <GoogleOAuthProvider clientId="544441269934-lu0k8ste87fb3j2k1kv4kb21m24070hh.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
