213502cf6e605e7bce4cb1f9b43d653f7e1f2b89
Anurag-Codes <anuragsinghyadav2658@gmail.com>
2026-08-10 05:52:55 +0530

    Added Vercel Analytics in frontend

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Analytics />
     {/* NAYA CODE: GoogleOAuthProvider add kiya gaya hai tera Client ID daal ke */}
     <GoogleOAuthProvider clientId="544441269934-lu0k8ste87fb3j2k1kv4kb21m24070hh.apps.googleusercontent.com">
       <App />
